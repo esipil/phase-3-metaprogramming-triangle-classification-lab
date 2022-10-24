c19c1a4a0499df8e7630fcb5c5ee0d684bba3497
Timothy <lemayiant4@gmail.com>
2022-10-24 23:34:22 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
-class Triangle
-  # write code here
+require 'pry'
+
+class Triangle 
+
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize (side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three 
+  end
+  def  kind
+    if side_one > 0 && side_two > 0 && side_three > 0 && side_one + side_two > side_three && side_two + side_three > side_one && side_one + side_three > side_two
+      if side_one == side_two &&  side_three == side_one 
+        :equilateral
+      elsif side_one == side_two && side_three != side_one || side_two == side_three && side_one!= side_two || side_three == side_one && side_two != side_three 
+        :isosceles
+      elsif side_one != side_two && side_two!= side_three && side_three != side_one
+        :scalene
+      end 
+    else 
+      raise TriangleError 
+    end   
+  end 
+  class TriangleError < StandardError 
+  end
 end
+binding.pry
